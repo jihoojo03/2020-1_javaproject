2efe265eea6813b586fc88628c702a437df92c16
SEBIN-LEE <61185549+SEBIN-LEE@users.noreply.github.com>
2020-05-27 22:27:48 +0900

    Update PanelTest.java

--- TheMafia/PanelTest.java
+++ TheMafia/PanelTest.java
@@ -12,5 +12,14 @@ public class PanelTest extends JFrame {
 	}
 
 	mb.add(screenMenu);
+	
+	p1 = new Jpanel("배경");
+	p2 = new Jpanel("인물");
+	p3 = new Jpanel("텍스트박스");
+	
+	p3.setLocationRelativeTo(null); //set frame mid
+	
+	
+	p1.add(
 
 }

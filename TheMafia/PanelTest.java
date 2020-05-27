b48f8fcd1ec2a946d97be666eaa905094c4a427b
SEBIN-LEE <61185549+SEBIN-LEE@users.noreply.github.com>
2020-05-27 22:20:49 +0900

    Update and rename panelTest.java to PanelTest.java

---
+++ TheMafia/PanelTest.java
@@ -0,0 +1,16 @@
+package TheMafia;
+import javax.swing.*;
+
+public class PanelTest extends JFrame {
+
+	PanelTest(){
+		setTitle("PanelTest");
+		setSize(800, 1280);
+    setLocation(300,200);
+		setVisible(true);
+    setResizable(false);
+	}
+
+		mb.add(screenMenu);
+
+}
